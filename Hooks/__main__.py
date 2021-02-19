d350835a75ddc737e0017ebc3b4e106dc720d643
Macro303 <Macro303@users.noreply.github.com>
2021-02-19 15:09:46 +1300

    Forgot to cleanup the threading func

--- Hooks/__main__.py
+++ Hooks/__main__.py
@@ -64,44 +64,6 @@ def main():
         LOGGER.info(f"Stopping script")
 
 
-def thread_func(poll_rate: int, hooks: List[str], testing: bool = False):
-    while True:
-        config = load_config(testing)
-        np_response = request_data(config)
-        LOGGER.debug(f"Neptune's Response: {np_response}")
-        if np_response:
-            new_players = []
-            np_players = [player['Username'] for player in np_response['Players'] if player['Username']]
-            for new_player in np_players:
-                if new_player not in config['Players'].keys():
-                    config['Players'][new_player] = {
-                        'Team': None
-                    }
-                    save_config(config, testing)
-                    new_players.append(new_player)
-            if np_response['Tick'] > config['Neptune\'s Pride']['Last Tick'] or testing:
-                player_stats = parse_player_stats(np_response, config)
-                LOGGER.debug(player_stats)
-
-                team_stats = parse_team_stats(np_response, config) if is_teamed(config) else None
-                LOGGER.debug(team_stats)
-
-                turn = int(np_response['Tick'] / config['Neptune\'s Pride']['Tick Rate'])
-
-                if 'teams' in hooks:
-                    post_results_on_teams(player_stats, team_stats, turn, np_response['Title'], config)
-                if 'discord' in hooks:
-                    post_results_on_discord(player_stats, team_stats, turn, np_response['Title'], config)
-                config['Neptune\'s Pride']['Last Tick'] = np_response['Tick']
-                save_config(config, testing)
-                LOGGER.info('Waiting for next turn...')
-        else:
-            break
-        if not np_response['Active'] or testing:
-            break
-        time.sleep(poll_rate * 60)
-
-
 if __name__ == '__main__':
     init_logger('Neptunes-Hooks')
     main()
