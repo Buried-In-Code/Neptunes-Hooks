ca3a402bd6ffcc1df1015931dd82bf3273a7d169
Buried-In-Code <6057651+Buried-In-Code@users.noreply.github.com>
2021-11-30 12:09:18 +1300

    Add rich to make fancy console outputs

--- neptunes_hooks/__main__.py
+++ neptunes_hooks/__main__.py
@@ -3,28 +3,32 @@ import time
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
+from rich.logging import RichHandler
+
+from neptunes_hooks.console import ConsoleLog
 from neptunes_hooks.discord_api import push_data as push_to_discord
 from neptunes_hooks.microsoft_teams_api import push_data as push_to_microsoft_teams
 from neptunes_hooks.neptunes_pride_api import pull_data
 from neptunes_hooks.settings import PlayerConfig, Settings
 from neptunes_hooks.utils import parse_player_stats, parse_team_stats, safe_get
 
-LOGGER = logging.getLogger("Neptunes-Hooks")
-TIMEOUT = 100
+CONSOLE = ConsoleLog("Neptunes-Hooks")
 SETTINGS = Settings()
 
 
 def setup_logging(debug: bool = False):
     Path("logs").mkdir(exist_ok=True)
     file_handler = logging.FileHandler("logs/Neptunes-Hooks.log")
-    file_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+    file_handler.setFormatter(
+        logging.Formatter(fmt="%(asctime)s [%(levelname)-8s] {%(name)s} | %(message)s", datefmt="[%Y-%m-%d %H:%M:%S]")
+    )
 
-    stream_handler = logging.StreamHandler()
+    stream_handler = RichHandler(rich_tracebacks=True, show_path=False)
     stream_handler.setLevel(logging.INFO if debug else logging.WARNING)
 
     logging.basicConfig(
-        format="[%(asctime)s] [%(levelname)-8s] {%(name)s} | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format="{%(name)s} | %(message)s",
+        datefmt="[%Y-%m-%d %H:%M:%S]",
         level=logging.NOTSET,
         handlers=[file_handler, stream_handler],
     )
@@ -43,6 +47,8 @@ def main():
     args = get_arguments()
     setup_logging(debug=args.debug)
 
+    CONSOLE.panel("Welcome to Neptune's Pride", expand=False, style="bold blue", justify="center")
+
     response = pull_data(game_id=SETTINGS.game_id, api_code=SETTINGS.api_code)
     while response and response["Active"]:
         players = [x["Username"] for x in response["Players"] if x["Username"]]
@@ -51,11 +57,16 @@ def main():
                 SETTINGS.players.append(PlayerConfig(username=new_player))
                 SETTINGS.save()
 
+        if not args.microsoft_teams and not args.discord:
+            break
+
         if response["Tick"] > SETTINGS.last_tick or args.debug:
+            turn = int(response["Tick"] / SETTINGS.tick_rate)
+            CONSOLE.rule(f"Turn {turn:02}", style="blue")
+
             player_stats = parse_player_stats(response, SETTINGS.players)
             team_stats = parse_team_stats(response, SETTINGS.players)
 
-            turn = int(response["Tick"] / SETTINGS.tick_rate)
             if args.microsoft_teams:
                 webhook = safe_get([x for x in SETTINGS.webhooks if x.service == "Microsoft Teams"])
                 if webhook:
@@ -67,7 +78,7 @@ def main():
 
             SETTINGS.last_tick = response["Tick"]
             SETTINGS.save()
-            LOGGER.info("Waiting for next turn...")
+            CONSOLE.print("Waiting for next turn...")
         if args.debug:
             break
         time.sleep(args.poll * 60)
